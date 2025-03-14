7457965e69b7539ea83170bfc699136ff3e07ce6
183Natasha <168049595+183Natasha@users.noreply.github.com>
2025-03-14 21:22:59 +0500

    реализован связный список

--- 5-map/map.ts
+++ 5-map/map.ts
@@ -1,78 +1,100 @@
+class ListNode {
+    constructor(
+        public key: string,
+        public value: number,
+        public next: ListNode | null = null,
+    ) { }
+}
+
+
 class MyMap {
-    buckets: Array<{ key: string, value: number }[]>;
+    buckets: Array<ListNode | null>;
     private bucketsSize: number;
 
     constructor(size: number = 7) {
-        // Инициализация структуры
-        this.buckets = new Array(size).fill(null).map(() => [])
+        this.buckets = new Array(size).fill(null)
         this.bucketsSize = size;
     }
 
     hashFunction(key: string): number {
         let hash = 0;
         for (let i = 0; i < key.length; i++) {
-            hash += key.charCodeAt(i); // Суммируем коды символов ключа
+            hash += key.charCodeAt(i);
         }
-        // console.log(hash % this.bucketsSize)
-        return hash % this.bucketsSize; // Ограничиваем хэш размером бакетов
+        return hash % this.bucketsSize;
     }
 
 
 
-    set(key: string, value: number) {
-        const index = this.hashFunction(key); // Вычисляем индекс бакета
-        const bucket = this.buckets[index]; // Получаем бакет
+    set(key: string, value: number): void {
+        const index = this.hashFunction(key);
+        let currentNode = this.buckets[index];
 
-        // Проверяем, есть ли уже такой ключ в бакете
-        for (let i = 0; i < bucket.length; i++) {
-            if (bucket[i].key === key) {
-                bucket[i].value = value; // Обновляем значение, если ключ уже существует
-                return;
-            }
+        if (currentNode == null) {
+            this.buckets[index] = new ListNode(key, value);
+            return
         }
-        bucket.push({ key, value })
-        // console.log(bucket)
+
+        while (currentNode) {
+            if (currentNode.key == key) {
+                currentNode.value = value;
+                return
+            }
+            if (!currentNode.next) break
+            currentNode = currentNode.next;
+        }
+        currentNode.next = new ListNode(key, value);
     }
 
     has(key: string): boolean {
-        const index = this.hashFunction(key); // Вычисляем индекс бакета
-        const bucket = this.buckets[index]; // Получаем бакет
-        for (let res of bucket) {
-            if (res.key == key) {
-                return true;
+        const index = this.hashFunction(key);
+        let currentNode = this.buckets[index];
+
+        while (currentNode) {
+            if (currentNode.key == key) {
+                return true
             }
+            currentNode = currentNode.next
         }
         return false
     }
 
-    delete(key: string):boolean {
-        const index = this.hashFunction(key); // Вычисляем индекс бакета
-        let bucket = this.buckets[index]; // Получаем бакет
+    delete(key: string): boolean {
+        const index = this.hashFunction(key);
+        let currentNode = this.buckets[index];
+        let previousNode: ListNode | null = null;
 
-        for (let i = 0; i < bucket.length; i++) {
-            if (bucket[i].key === key) {
-                bucket.splice(i, 1); // Удаляем пару, если ключ найден
-                return true;
+        while (currentNode) {
+            if (currentNode.key === key) {
+                if (previousNode) {
+                    previousNode.next = currentNode.next;
+                } else {
+                    this.buckets[index] = currentNode.next;
+                }
+                return true; 
             }
-        }
+            previousNode = currentNode;
+            currentNode = currentNode.next;
 
-        return false; // Ключ не найден
+        }
+        return false
     }
+
     get(key: string) {
-        const index = this.hashFunction(key); // Вычисляем индекс бакета
-        let bucket = this.buckets[index]; // Получаем бакет
+        const index = this.hashFunction(key); 
+        let currentNode = this.buckets[index]; 
 
-        for (let i = 0; i < bucket.length; i++) {
-            if (bucket[i].key === key) {
-                return bucket[i].value;
+        while (currentNode) {
+            if (currentNode.key === key) {
+                return currentNode.value;
             }
+            currentNode = currentNode.next
         }
-
-        return false; // Ключ не найден
+        return false; 
     };
 
     clear(size: number = 7) {
-        return this.buckets = new Array(size).fill(null).map(() => [])
+        return this.buckets = new Array(size).fill(null)
     }
 
 
@@ -83,15 +105,17 @@ let weatherMap = new MyMap();
 weatherMap.set('London', 20);
 weatherMap.set('Valencia', 30);
 weatherMap.set('Berlin', 25);
+weatherMap.set('Lndnoo', -15);
 
 // Пример получения данных
-// console.log(weatherMap.has('London')); 
+console.log(weatherMap.has('London')); 
 
-// weatherMap.hashFunction('London')
-// weatherMap.hashFunction('Valencia')
-// weatherMap.hashFunction('Berlin')
-console.log(weatherMap.buckets)
-console.log(weatherMap.has('Berlin')); 
-console.log(weatherMap.get('Berlin')); 
-console.log(weatherMap.delete('Berlin')); 
+weatherMap.hashFunction('London')
+weatherMap.hashFunction('Valencia')
+weatherMap.hashFunction('Berlin')
+console.log(weatherMap.buckets[2])
+// weatherMap.clear()
+console.log(weatherMap.has('Lndnoo'));
+console.log(weatherMap.get('Valencia'));
+console.log(weatherMap.delete('Berlin'));
 console.log(weatherMap.buckets)
